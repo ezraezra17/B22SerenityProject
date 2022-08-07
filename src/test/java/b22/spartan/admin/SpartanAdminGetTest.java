df1ba647e1acccb8cee9d8a145a391789a639a1f
eshazine <eshazine@gmail.com>
2022-08-07 15:49:47 +0300

    Serenit Test Imports Added

--- src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -3,13 +3,15 @@ package b22.spartan.admin;
 import static io.restassured.RestAssured.*;
 
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.SerenityRest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static net.serenitybdd.rest.SerenityRest.given;
 @SerenityTest
 public class SpartanAdminGetTest {
 
@@ -32,4 +34,21 @@ public class SpartanAdminGetTest {
                 .and().contentType(ContentType.JSON);
 
     }
+
+    @Test
+    public void getOneSpartan(){
+
+      given().accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .pathParam("id",15)
+                .when()
+                .get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .and().contentType(ContentType.JSON);
+
+    }
+
+
+
 }
